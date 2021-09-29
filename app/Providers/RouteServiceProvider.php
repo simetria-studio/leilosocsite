04f2dda3428e329639eb916c53cb6e50a9e3307a
felipe cruz <felipephplow@gmail.com>
2021-09-29 11:07:59 -0300

    a

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/';
+    public const HOME = 'myleilosoc';
 
     /**
      * The controller namespace for the application.
